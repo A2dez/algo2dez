3785fbccb9ebe665884a00f71b0474ec5f0c7d5e
cex sligo <cexsligo@cexs-MacBook-Pro.local>
2021-01-31 22:46:38 +0000

    extacting phones & barplotting them

--- read_carnegie_mellon_pronouncing_dictionary.R
+++ read_carnegie_mellon_pronouncing_dictionary.R
@@ -1,27 +1,71 @@
 
-install.packages('tidyverse')
+# install.packages('tidyverse')
 library(tidyverse)
-data <- read_delim("https://github.com/Alexir/CMUdict/blob/master/cmudict-0.7b", delim = "  ", skip = 126)
-data
 
-data <- read_delim("http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b", delim = "  ")
-data
-View(data)
+# For some reason I can't read in the original data'
+# data <- read_delim("https://github.com/Alexir/CMUdict/blob/master/cmudict-0.7b", delim = "  ", skip = 126)
+
+# data <- read_delim("http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b", delim = "  "
+# 
+# cp /Users/cexsligo/data_des/csv_files/Carnegie_Mellon_Original_Cleaned.csv  Carnegie_Mellon_Original_Cleaned.csv 
+
+#so here is a version which I edited for another project, getting rid of the top 126 rows
+#and separating out the spellings from the pron(unciation)s
+cmu <- read_csv('Carnegie_Mellon_Original_Cleaned.csv')
 
 
-# move to the project directory
-cd Projects/website
 
-# initiate the upstream tracking of the project on the GitHub repo
-git remote add origin https://github.com/hansenjohnson/website.git
+# 0. clean the data -------------------------------------------------------
 
-# pull all files from the GitHub repo (typically just readme, license, gitignore)
-git pull origin master
+#this is hilarious. the letters 'NA' form an entry and R read is as NA
+x <- which(is.na(cmu$word))
+# cmu[x,]
 
-# set up GitHub repo to track changes on local machine
-git push -u origin master
+# A tibble: 1 x 3
+#   word  phonForm length
+#   <chr> <chr>     <int>
+# 1 NA    N AA1        NA
 
-git remote add origin https://github.com/A2dez/algo2dez
-git status
+cmu[x, 1] = 'na'
+
+#remove  nas in the phone section
+cmu <- filter(cmu, is.na(cmu$phonForm)  == F)
+
+
+#make letters lowercase 
+cmu$word <- tolower(cmu$word)
+
+#add a column for number of letters
+cmu$length <- str_count(cmu$word) 
+
+
+
+  #lets get a list of phones & make a table
+phoneset_frequencies <- str_c(cmu$phonForm, collapse = ' ') %>% 
+  str_split(' ') %>% table() 
+
+#dfrm it 
+phoneset_freqs <- data.frame(phoneset_frequencies)
+
+#sneaky way of separating out vowels and consonants (extract the number)
+phoneset_freqs$vorc <- str_extract(phoneset_freqs$., '\\d')
+
+
+phoneset_freqs <- phoneset_freqs %>% 
+  arrange(vorc, Freq) 
+
+#order phones by stress pattern
+phoneset_freqs$. <- fct_reorder2(phoneset_freqs$., phoneset_freqs$vorc, phoneset_freqs$Freq)
+ 
+ 
+phoneset_freqs %>% 
+  ggplot( aes (x = ., 
+               y = Freq, 
+               fill = vorc)) +
+  geom_bar(stat = 'identity') +
+  coord_flip() +
+  facet_wrap(vars(vorc),
+             scales = 'free')
+
 
 
